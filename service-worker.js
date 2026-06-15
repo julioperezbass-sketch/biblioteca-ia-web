21f9f329f0816839f515a4b7768b4b845f8ddc6d
julioperezbass-sketch <julioperezbass@gmail.com>
2026-06-15 20:34:08 +0200

    Publica Biblioteca IA Musical

---
+++ service-worker.js
@@ -0,0 +1,74 @@
+const CACHE_NAME = "biblioteca-ia-musical-v2";
+const APP_SHELL = [
+  "/",
+  "/index.html",
+  "/biblioteca-ia-web/manifest.webmanifest",
+  "/apoyar.html",
+  "/contacto.html",
+  "/biblioteca-ia-web/icons/icon-192.png",
+  "/biblioteca-ia-web/icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) =>
+        Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+      )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  const request = event.request;
+
+  if (request.method !== "GET") {
+    return;
+  }
+
+  const requestUrl = new URL(request.url);
+
+  if (request.mode === "navigate") {
+    event.respondWith(
+      fetch(request)
+        .then((response) => {
+          if (response.ok) {
+            const responseCopy = response.clone();
+            caches.open(CACHE_NAME).then((cache) => cache.put(request, responseCopy));
+          }
+          return response;
+        })
+        .catch(() => caches.match("/index.html"))
+    );
+    return;
+  }
+
+  if (requestUrl.origin !== self.location.origin) {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      return fetch(request).then((networkResponse) => {
+        if (networkResponse && networkResponse.ok) {
+          const responseCopy = networkResponse.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(request, responseCopy));
+        }
+
+        return networkResponse;
+      });
+    })
+  );
+});
